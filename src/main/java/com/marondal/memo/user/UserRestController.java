a58cee8c9bc10036046df2fc05f80bd8831cb3c2
megaIt202 <megait202@gmailcom>
2024-03-19 18:11:18 +0900

    [김인규]  - jsp 설정  - database 설정  - 회원가입 화면 구성  - 회원가입 API 및 ajax 구현
    #1

---
+++ src/main/java/com/marondal/memo/user/UserRestController.java
@@ -0,0 +1,39 @@
+package com.marondal.memo.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marondal.memo.user.service.UserService;
+
+@RestController // @Controller + @ReseponseBody
+public class UserRestController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/user/join")
+	public Map<String, String> join(
+			@RequestParam("loginId") String loginId
+			, @RequestParam("password") String password
+			, @RequestParam("name") String name
+			, @RequestParam("email") String email) {
+		
+		int count = userService.addUser(loginId, password, name, email);
+		
+		Map<String, String> resultMap = new HashMap<>();
+		
+		if(count == 1) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+	}
+
+}
---
+++ src/main/java/com/marondal/memo/user/service/UserService.java
@@ -0,0 +1,18 @@
+package com.marondal.memo.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.memo.user.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public int addUser(String loginId, String password, String name, String email) {
+		return userRepository.insertUser(loginId, password, name, email);
+	}
+
+}
