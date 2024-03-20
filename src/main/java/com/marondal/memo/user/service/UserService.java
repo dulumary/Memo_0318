510521139549528e99b66a20cb87428bd778dc02
megaIt202 <megait202@gmailcom>
2024-03-20 16:40:11 +0900

    [김인규]  - 회원가입 비밀번호 기능 구현
    #1

--- src/main/java/com/marondal/memo/user/service/UserService.java
+++ src/main/java/com/marondal/memo/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.marondal.memo.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marondal.memo.common.EncrytUtils;
 import com.marondal.memo.user.repository.UserRepository;
 
 @Service
@@ -12,7 +13,10 @@ public class UserService {
 	private UserRepository userRepository;
 	
 	public int addUser(String loginId, String password, String name, String email) {
-		return userRepository.insertUser(loginId, password, name, email);
+		
+		String encryptPassword = EncrytUtils.md5(password);
+		
+		return userRepository.insertUser(loginId, encryptPassword, name, email);
 	}
 
 }
