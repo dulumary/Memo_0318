67c341ce3ccb7aee8fda6df07a3eaef75d1a0d17
megaIt202 <megait202@gmailcom>
2024-03-22 18:45:53 +0900

    [김인규]  - 메모 작성 기능 완료

---
+++ src/main/java/com/marondal/memo/post/repository/PostRepository.java
@@ -0,0 +1,22 @@
+package com.marondal.memo.post.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.marondal.memo.post.domain.Post;
+
+@Mapper
+public interface PostRepository {
+	
+	public int insertPost(
+			@Param("userId") int userId
+			, @Param("title") String title
+			, @Param("contents") String contents);
+	
+	public List<Post> selectPostList(@Param("userId") int userId);
+	
+	public Post selectPost(@Param("id") int id);
+
+}
